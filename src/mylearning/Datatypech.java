1300b757ef1d4436257ed18b7f26120370aebe86
haris <haris@host.docker.internal>
2023-11-09 15:16:09 -0500

    ch datatype assignment

---
+++ src/mylearning/Datatypech.java
@@ -0,0 +1,15 @@
+package mylearning;
+
+public class Datatypech {
+
+	public static void main(String[] args) {
+		char ch = '\u0907';	
+		char ch1 = '\u0937';
+		char ch2 = '\u093F';
+		char ch3 = '\u0925';
+		char ch4 = '\u093E';
+		System.out.println(ch+""+ch1+""+ch2+""+ch3+""+ch4);
+
+	}
+
+}
