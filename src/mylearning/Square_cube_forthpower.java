47f430e46ada2b8d5b75e39f84919dcb33c6a381
haris <haris@host.docker.internal>
2023-11-14 16:05:47 -0500

    updates

--- src/mylearning/Square_cube_forthpower.java
+++ src/mylearning/Square_cube_forthpower.java
@@ -3,16 +3,17 @@ package mylearning;
 import java.util.Scanner;
 public class Square_cube_forthpower
 {
-public static void main(String[] args)
-{
-    Scanner in = new Scanner(System.in);
-    System.out.println("Input the value:");
-    double val = in.nextDouble();
-
-    System.out.printf("square:",Math.pow(val, 2));
-    //System.out.printf("Cube: ", Math.pow(val, 3));
-    //System.out.printf("Fourth power:", Math.pow(val, 4));
+	 public static void main(String[] args)
+	    {
+	        Scanner in = new Scanner(System.in);
+	        System.out.print("Enter the value: ");
+	        double val = in.nextDouble();
+	        double square=Math.pow(val, 2);
+	        double cube=Math.pow(val, 3);
+	        double fourth_power=Math.pow(val, 4);
+	        System.out.println("Square: "+square);
+	        System.out.println("Cube: "+cube);
+	        System.out.println("fourth power: "+fourth_power);
+	       
+	    }
 }
-}
-
-
