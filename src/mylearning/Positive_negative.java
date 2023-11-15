c2c2af221922751aa89f0d4ea0a64e15f95b520d
haris <haris@host.docker.internal>
2023-11-15 15:17:50 -0500

    update

--- src/mylearning/Positive_negative.java
+++ src/mylearning/Positive_negative.java
@@ -6,7 +6,7 @@ public class Positive_negative {
 
 	public static void main(String[] args) {
 		Scanner obj = new Scanner(System.in);
-		System.out.println("Enter number a number:");
+		System.out.println("Enter a number:");
 		int number = obj.nextInt();
 		if(number>0) {
 			System.out.println("Number is Possitive:"+number);
