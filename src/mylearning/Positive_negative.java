7ccd04de0a653f0cf2b619aeaf7f1bca4aa11a00
haris <haris@host.docker.internal>
2023-11-15 14:02:33 -0500

    Number is Positive or Negative

---
+++ src/mylearning/Positive_negative.java
@@ -0,0 +1,20 @@
+package mylearning;
+
+import java.util.Scanner;
+
+public class Positive_negative {
+
+	public static void main(String[] args) {
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter number a number:");
+		int number = obj.nextInt();
+		if(number>0) {
+			System.out.println("Number is Possitive:"+number);
+		}else if (number<0){
+			System.out.println("Number is Negative:"+number);
+		}else 
+			System.out.println("Number is Zero:"+number);
+		
+	}
+
+}
