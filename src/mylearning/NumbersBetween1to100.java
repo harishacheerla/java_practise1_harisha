b352ee4c7a81d84f1eafe1bc218936b7d81f9612
haris <haris@host.docker.internal>
2023-11-20 13:23:47 -0500

    commit

--- src/mylearning/NumbersBetween1to100.java
+++ src/mylearning/NumbersBetween1to100.java
@@ -5,20 +5,21 @@ import java.util.Scanner;
 public class NumbersBetween1to100 {
 
 	public static void main(String[] args) {
-		Scanner obj1 = new Scanner(System.in);
-        System.out.print("Enter numbers between 1 to 100:");
-        int num1=obj1.nextInt();
-        for(num1=0;num1<=100;num1++) {          
+		//Scanner obj1 = new Scanner(System.in);
+        //System.out.println("Enter numbers between 1 to 100:");
+        //int num1=obj1.nextInt();
+        for(int num1=0;num1<=100;num1++) {          
          if((num1 % 3==0)&&(num1 % 5==0)) {
         	 System.out.println("Number is divisible by 3 & 5: " + num1);
          } else if (num1 % 3 == 0) {
-        	 System.out.println("Number is divisible by 3: " + num1);
+        	 System.out.println("Number is divisible by 3:" + num1);
          } else if (num1 % 5 == 0) {
-        	 System.out.println("Number is divisible by 5: " + num1);
+        	 System.out.println("Number is divisible by 5:" + num1);
         }
         }
 	}
 
 	}
+
 
 
