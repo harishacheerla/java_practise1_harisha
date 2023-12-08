514d805478b0014d77d08df89797f37a8803e319
haris <haris@host.docker.internal>
2023-12-07 22:49:28 -0500

    updates

--- src/objects/StaticandNonstatic.java
+++ src/objects/StaticandNonstatic.java
@@ -1,11 +1,14 @@
 package objects;
 
+import java.util.Scanner;
+
 public class StaticandNonstatic {
 
 
 	    public static void main(String args[]) {
 	    	StaticandNonstatic s= new StaticandNonstatic();
 	        s.add(10,20);
+	        int result=s.pssitiveornegative();
 	    }
 
 	    public void add(int x ,int y) {
@@ -13,5 +16,19 @@ public class StaticandNonstatic {
 	        int b = y;
 	        int c = a + b;
 	        System.out.println("addition " + c);
+	    }
+	    //Non parameterized method
+	  public int pssitiveornegative() {
+	    	int number=-1;
+	    		
+	    		if(number>0) {
+	    			System.out.println("Number is Possitive:"+number);
+	    		}else if (number<0){
+	    			System.out.println("Number is Negative:"+number);
+	    		}else 
+	    			System.out.println("Number is Zero:"+number);
+				return number;
+	    		
+	    	
 	    }
 	}
