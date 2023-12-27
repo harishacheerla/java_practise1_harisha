4e90b0f9fb56f5da21b4b04eb7170c3b7f7f5b98
haris <haris@host.docker.internal>
2023-12-27 15:02:48 -0500

    constructor

---
+++ src/objects/Constructor2.java
@@ -0,0 +1,27 @@
+package objects;
+
+public class Constructor2 {
+	
+	Constructor2(){
+		this(1);
+	   Constructor2();
+		
+		  }
+
+	Constructor2(int i ){
+		  
+		  }
+		  
+		  public void Constructor2(){
+		    System.out.println("hello");
+		  }
+
+	
+	public static void main(String[] args) {
+		Constructor2 test=new Constructor2();
+	
+		
+
+	}
+
+}
